7c49b1c7cf1be42ed8b75cd9ff21ea0bb35fda71
x1783290948 <x1783290948@gmail.com>
2026-01-22 01:01:09 +0800

    Level-0: Rename and Greet

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# JoJo User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/JoJo.java
@@ -0,0 +1,13 @@
+public class JoJo {
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
+
+        System.out.println(horizontalLine);
+        System.out.println(" Hello! I'm Todo");
+        System.out.println(" What can I do for you?");
+        System.out.println(horizontalLine);
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
