6692f9f78b48b4c775387350a1507e130d280857
x1783290948 <x1783290948@gmail.com>
2026-01-22 01:12:51 +0800

    Level-1: Echo

--- src/main/java/JoJo.java
+++ src/main/java/JoJo.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
 public class JoJo {
     public static void main(String[] args) {
         String horizontalLine = "____________________________________________________________";
 
         System.out.println(horizontalLine);
-        System.out.println(" Hello! I'm Todo");
+        System.out.println(" Hello! I'm JoJo");
         System.out.println(" What can I do for you?");
         System.out.println(horizontalLine);
 
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+//            if (input.equalsIgnoreCase("jojo")) {
+//                System.out.println(horizontalLine);
+//                System.out.println("It's me!");
+//            }
+
+            System.out.println(horizontalLine);
+            System.out.println(" " + input); // Repeat the user's input
+            System.out.println(horizontalLine);
+        }
+
+        System.out.println(horizontalLine);
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);
     }
